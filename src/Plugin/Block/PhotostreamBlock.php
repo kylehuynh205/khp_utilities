782532c397c520e7681c7aeac8a3cc8be4738859
Khai Huynh <huynhk@yorku.ca>
2020-04-09 11:15:22 -0400

    block photostream established

---
+++ src/Plugin/Block/PhotostreamBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\utilities\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'PhotostreamBlock' block.
+ *
+ * @Block(
+ *  id = "photostream_block",
+ *  admin_label = @Translation("Photostream"),
+ * )
+ */
+class PhotostreamBlock extends BlockBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+        $nids = \Drupal::entityQuery('node')
+                ->condition('type', 'flickr_photo')
+                ->pager('70')
+                ->execute();
+        
+        $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+        return [
+            'results' => [
+                '#theme' => 'photostream_block',
+                '#photos' => $nodes,
+                '#path' => base_path()
+            ],
+            'pager' => [
+                '#type' => 'pager',
+            ],
+        ];
+        //return $build;
+    }
+
+}
